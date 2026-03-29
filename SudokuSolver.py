deb9110bbedf7a37bc5587af9e11493048e51ae5
Thalia <mythaliasm@gmail.com>
2026-03-29 17:01:39 +0300

    Add user input for the Sudoku board

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,16 +1,12 @@
-board = [
-  [5,3,0, 0,7,0, 0,0,0],
-  [6,0,0, 1,9,5, 0,0,0],
-  [0,9,8, 0,0,0, 0,6,0],
+#Empting the board
+board = [[0]*9 for _ in range(9)]
 
-  [8,0,0, 0,6,0, 0,0,3],
-  [4,0,0, 8,0,3, 0,0,1],
-  [7,0,0, 0,2,0, 0,0,6],
+#Fill the board
+for i in range(9):
+    print("Give line ", i+1)
+    for j in range(9):
+        board[i][j] = int(input(f"Position ({i+1}, {j+1}):"))
 
-  [0,6,0, 0,0,0, 2,8,0],
-  [0,0,0, 4,1,9, 0,0,5],
-  [0,0,0, 0,8,0, 0,7,9],
-]
 def print_board(board):
     for i in range(9):
         print(board[i])
